85d4f906d703fb3df47f03c1f6b6278df1036962
BL4CKM4G3 <alvaro_rangel@outlook.com>
2020-08-07 04:10:37 -0300

    updated template and added angular-in-memory-api database

---
+++ src/app/in-memory-database.ts
@@ -0,0 +1,17 @@
+import { InMemoryDbService } from "angular-in-memory-web-api";
+
+export class InMemoryDatabase implements InMemoryDbService {
+  createDb() {
+    const categories = [
+      { id: 1, name: "Lazer", description: "" },
+      { id: 2, name: "Tech", description: "" },
+      { id: 3, name: "Code", description: "" },
+      { id: 4, name: "Design", description: "" },
+      { id: 5, name: "Rest", description: "" },
+      { id: 6, name: "Eat", description: "" },
+      { id: 7, name: "Trip", description: "" },
+      { id: 8, name: "Chill", description: "" },
+    ];
+    return {categories};
+  }
+}
