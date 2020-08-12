889c2eff8bdce977df9151fdb274f31e39bc8032
BL4CKM4G3 <alvaro_rangel@outlook.com>
2020-08-12 02:49:28 -0300

    updates in category form

--- src/app/pages/categories/category-form/category-form.component.ts
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterContentChecked } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
+import { ActivatedRoute, Router } from "@angular/router";
+
+import { switchMap } from "rxjs/operators";
+import toastr from "toastr";
+import { Category } from '../shared/category.model';
+import { CategoryService } from '../shared/category.service';
 
 @Component({
   selector: 'app-category-form',
@@ -7,9 +14,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryFormComponent implements OnInit {
 
-  constructor() { }
+  currentAction: string;
+  categoryForm: FormGroup;
+  pageTitle: string;
+  serverErrorMessages: string[] = null;
+  submittingForm: boolean = false;
+  category: Category = new Category();
+
+  constructor(
+    private readonly categoryService: CategoryService,
+    private readonly activatedRoute: ActivatedRoute,
+    private readonly router: Router,
+    private readonly formBuilder: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.setCurrentAction();
+    this.buildCategoryForm();
+    this.loadCategory();
+  }
+
+  ngAfterContentChecked(): void {
+    //Called after every check of the component's or directive's content.
+    //Add 'implements AfterContentChecked' to the class.
+    
+  }
+
+  // private methods
+  private setCurrentAction() {
+    if (this.activatedRoute.snapshot.url[0].path == "new") {
+      this.currentAction = "new";
+    } else {
+      this.currentAction = "edit";
+    }
+  }
+
+  private buildCategoryForm() {
+    this.categoryForm = this.formBuilder.group({
+      id: [null],
+      name: [null, [Validators.minLength(2)]],
+      description: [null]
+    });
+  }
+
+  private loadCategory() {
+    if (this.currentAction == "edit") {
+      this.activatedRoute.paramMap.pipe(
+        switchMap(params => this.categoryService.getById(params.get("id")))
+      );
+    }
   }
 
 }
